86423c89759f9c8916716d0a065d3ed38961f845
Michal Kaszubowski <MrKowser@gmail.com>
2015-12-15 01:37:17 -0800

    failing login branch

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -22,5 +22,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert_not flash.nil?
+    assert is_logged_in?
   end
 end
